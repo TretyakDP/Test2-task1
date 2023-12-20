9ba189632286a9b1020c2dbd7dca844257d4a62f
DmitriyTR <skate1233@mail.ru>
2023-12-20 21:20:48 +0300

    Добавьте файлы проекта.

---
+++ Test2 task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        List<int> numbers = Enumerable.Range(1, 8).Select(_ => random.Next(1, 100)).ToList();
+        var sortedNumbers = numbers.OrderBy(x => x).ToList();
+        int middleIndex = (int)Math.Ceiling(sortedNumbers.Count / 2.0);
+        var secondHalf = sortedNumbers.Skip(middleIndex).ToList();
+        var result = secondHalf.Select(x => x * x).OrderByDescending(x => x).ToList();
+        Console.WriteLine("Исходная коллекция: " + string.Join(", ", numbers));
+        Console.WriteLine("Отсортированная вторая половина, элементы в квадрате: " + string.Join(", ", result));
+        Console.ReadLine();
+    }
+}
